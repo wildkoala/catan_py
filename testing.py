8f6a754638df82a58110b51188d6b83047b631d0
wildkoala <twiggy.gordon1@gmail.com>
2020-02-20 14:56:04 +0000

    Catan file and testing file modified

--- testing.py
+++ testing.py
@@ -1,16 +1,6 @@
-
-
-
-
-
-
-
-
-
-
-
-
-import catan_py 
+from catan_py import roll_dice, robber # if i try to import the whole file it won't run (only goes if main)
+import classes
+import items
 
 
 def test_sum():
